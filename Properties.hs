3e9ee934c814641347df8c2dbdfb2103fa73fb84
Brent Yorgey <byorgey@cis.upenn.edu>
2008-12-20 18:56:42 +0000

    more cleanup of Properties

--- Properties.hs
+++ Properties.hs
@@ -106,7 +106,6 @@ main = do
             , ("splitPlaces/preserve",          qc prop_splitPlaces_preserve)
             ]
 
--- The default splitting strategy is the identity.
 prop_default_id :: [Elt] -> Bool
 prop_default_id l = split defaultSplitter l == [l]
 
@@ -290,30 +289,3 @@ mInit :: [a] -> [a]
 mInit [] = []
 mInit [x] = []
 mInit (x:xs) = x : init xs
-
-{-
--- | split at regular intervals
-chunk :: Int -> [a] -> [[a]]
-chunk _ [] = [[]]
-chunk n xs = y1 : chunk n y2
-  where
-    (y1, y2) = splitAt n xs
--}
-
--- for testing, code from vixey
-{-
-match [] string = Just string
-match (_:_) [] = Nothing
-match (p:ps) (q:qs) | p == q = match ps qs
-match (_:_)  (_:_)  | otherwise = Nothing
-
-chopWith delimiter (match delimiter -> Just tail) = return ([], tail)
-chopWith delimiter (c:cs) = chopWith delimiter cs >>= \(head, tail) ->
-                              return (c:head, tail)
-chopWith delimiter [] = Nothing
--- note: chopWith could be make 'more efficient' i.e. remove the >>=\-> bit
---       by adding an accumulator
-
-
-unintercalate delimiter = unfoldr (chopWith delimiter)
--}
